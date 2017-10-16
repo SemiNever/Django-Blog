b5c53e503a1ef00d4e764e3467674f4bd4e7f3c9
Semi <781419433@qq.com>
2017-10-16 17:23:30 +0800

    实现文章的分页,应用ListView 和 DetailView改写

--- blog/templatetags/blog_tags.py
+++ blog/templatetags/blog_tags.py
@@ -1,5 +1,6 @@
 from ..models import  Post
 from django import template
+from django.db.models.aggregates import Count
 from ..models import Post, Category
 register =template.Library()
 
@@ -12,3 +13,8 @@ def archives():
 @register.simple_tag
 def get_categories():
     return Category.objects.all()
+@register.simple_tag
+def get_categories():
+    # 记得在顶部引入 count 函数
+    # Count 计算分类下的文章数，其接受的参数为需要计数的模型的名称
+    return Category.objects.annotate(num_posts=Count('post')).filter(num_posts__gt=0)
